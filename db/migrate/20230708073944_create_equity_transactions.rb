99bb494f1ac7cd744ac5fc7ad24e8147766c18b8
Ashwini Sukale <ashwinisukale16@gmail.com>
2023-07-10 14:15:14 +0530

    resolved the conflicts

---
+++ db/migrate/20230708073944_create_equity_transactions.rb
@@ -0,0 +1,14 @@
+class CreateEquityTransactions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :equity_transactions do |t|
+      t.integer :activity_id
+      t.datetime :date
+      t.string :transaction_type
+      t.string :method
+      t.float :amount
+      t.float :balance
+
+      t.timestamps
+    end
+  end
+end
